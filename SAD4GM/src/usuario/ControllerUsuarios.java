c9aef28f0e68a20fd29e5f72e7d77575df68e9e3
caiosbl <caio.lira@ccc.ufcg.edu.br>
2017-10-24 15:12:27 -0300

    Adicionado Métodos de Controle de Usuários

---
+++ SAD4GM/src/usuario/ControllerUsuarios.java
@@ -0,0 +1,62 @@
+package usuario;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.management.RuntimeErrorException;
+
+public class ControllerUsuarios {
+	private Map<String, Usuario> usuarios;
+
+	public ControllerUsuarios() {
+		this.usuarios = new HashMap<>();
+	}
+
+	private void validaId(String id) {
+		if (!usuarios.containsKey(id)) {
+			throw new RuntimeErrorException(null, "Usuário Não Cadastrado!");
+		}
+	}
+
+	public void adicionaUsuario(String nome, String id, String supervisor) {
+		Usuario usuario = new Usuario(nome, id, supervisor);
+		usuarios.put(usuario.getId(), usuario);
+	}
+
+	public Usuario buscaUsuario(String id) {
+		validaId(id);
+		return usuarios.get(id);
+
+	}
+
+	public void atualizaUsuario(String id, String dado, String novoValor) {
+		validaId(id);
+
+		Usuario usuario = usuarios.get(id);
+
+		switch (dado.toLowerCase()) {
+
+		case "nome":
+			usuario.setNome(novoValor);
+			break;
+		case "id":
+			Usuario usuarioTemporario = usuario;
+			usuarioTemporario.setId(novoValor);
+			removerUsuario(id);
+			usuarios.put(usuarioTemporario.getId(), usuarioTemporario);
+			break;
+		case "supervisor":
+			usuario.setSupervisor(novoValor);
+
+		default:
+			throw new RuntimeException("Novo valor inválido!");
+		}
+
+	}
+
+	public void removerUsuario(String id) {
+		validaId(id);
+		usuarios.remove(id);
+	}
+
+}
