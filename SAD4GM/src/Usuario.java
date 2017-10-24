b65cb42e2f3d011697e9e0fb93ce6961231aa09a
caiosbl <caio.lira@ccc.ufcg.edu.br>
2017-10-24 14:33:10 -0300

    Corpo de Usuário Iniciado

---
+++ SAD4GM/src/Usuario.java
@@ -0,0 +1,45 @@
+
+public class Usuario {
+
+	private String nome;
+	private String id;
+	private String supervisor;
+
+	public Usuario(String nome, String id, String supervisor) {
+		this.nome = nome;
+		this.id = id;
+		this.supervisor = supervisor;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getSupervisor() {
+		return supervisor;
+	}
+
+	public void setSupervisor(String supervisor) {
+		this.supervisor = supervisor;
+	}
+
+	@Override
+	public String toString() {
+		return "DADOS DO USUÁRIO:" + System.lineSeparator() + "Usuário: " + nome + System.lineSeparator() + "Id: " + id
+				+ System.lineSeparator() + "Supervisor: " + supervisor;
+
+	}
+
+}
