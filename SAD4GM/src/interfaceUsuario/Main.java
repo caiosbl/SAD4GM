b64c585c6e99cf4c87558168eb912e64efcf9821
caiosbl <caio.lira@ccc.ufcg.edu.br>
2017-10-25 11:21:58 -0300

    Alterações em Interface Provisória

--- SAD4GM/src/interfaceUsuario/Main.java
+++ SAD4GM/src/interfaceUsuario/Main.java
@@ -4,8 +4,6 @@ import java.util.Scanner;
 
 import sistema.Sistema;
 
-
-
 /**
  * UNIVERSIDADE FEDERAL DE CAMPINA GRANDE - LABORATÓRIO DESIDES SISTEMA SAD4GM
  * 
@@ -16,19 +14,28 @@ import sistema.Sistema;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println(opcoes());
-		Sistema sistema = new Sistema ();
-		cadastraUsuario(sistema);
+		Scanner sc = new Scanner(System.in);
+	
+		Sistema sistema = new Sistema();
+		String entrada = "";
+		
+		do {
+			System.out.println(opcoes());
+			System.out.print("OPÇÃO: ");
+			entrada = sc.nextLine();
+			selectOpcao(entrada, sistema);
+		
+		}
+		while(!entrada.equals("5"));
 	}
-	
-	
-	public static void selectOpcao(String opcao,Sistema sistema) {
+
+	public static void selectOpcao(String opcao, Sistema sistema) {
 		switch (opcao) {
 		case "1":
 			cadastraUsuario(sistema);
 			break;
 		case "2":
-			
+			atualizaUsuario(sistema);
 
 		default:
 			break;
@@ -38,13 +45,14 @@ public class Main {
 	public static String opcoes() {
 		String opcoes = "SELECIONE UMA OPÇÃO: " + System.lineSeparator() + "1 - CADASTRAR UM USUÁRIO"
 				+ System.lineSeparator() + "2 - ATUALIZAR UM USUÁRIO" + System.lineSeparator()
-				+ "3 - REMOVER UM USUÁRIO" + System.lineSeparator() + "4 - BUSCAR UM USUÁRIO" +
-				System.lineSeparator();
+				+ "3 - REMOVER UM USUÁRIO" + System.lineSeparator() + "4 - BUSCAR UM USUÁRIO" + System.lineSeparator() +
+				"5 - SAIR" + System.lineSeparator();
 
 		return opcoes;
 	}
 
-	public static  void cadastraUsuario(Sistema sistema) {
+	public static void cadastraUsuario(Sistema sistema) {
+		System.out.println("CADASTRAR UM USUÁRIO"  + System.lineSeparator());
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Nome: ");
 		String nome = sc.nextLine();
@@ -52,10 +60,26 @@ public class Main {
 		String id = sc.nextLine();
 		System.out.print("Auditor: ");
 		String auditor = sc.nextLine();
-		sc.close();
+	
+		System.out.println(System.lineSeparator() + sistema.cadastrarUsuario(nome, id, auditor) + System.lineSeparator());
+	
 
-		System.out.println( sistema.cadastrarUsuario(nome, id, auditor));
-
+	}
+	
+	public static void atualizaUsuario(Sistema sistema) {
+		System.out.println("ATUALIZAR UM USUÁRIO " + System.lineSeparator());
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("ID DO USUÁRIO: ");
+		String id = sc.nextLine();
+		System.out.print("DADO A SER ATUALIZADO: ");
+		String dado= sc.nextLine();
+		System.out.print("NOVO VALOR DO DADO: ");
+		String novoValor = sc.nextLine();
+		
+		
+		System.out.println(System.lineSeparator() + sistema.atualizarUsuario(id, dado, novoValor) + System.lineSeparator());
+		
 	}
 
 }
