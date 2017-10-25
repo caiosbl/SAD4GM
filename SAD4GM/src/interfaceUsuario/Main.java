418f078c799e6047401f4c788f85d308fefc615a
caiosbl <caio.lira@ccc.ufcg.edu.br>
2017-10-25 08:21:05 -0300

    Sem interface por enquanto

--- SAD4GM/src/interfaceUsuario/Main.java
+++ SAD4GM/src/interfaceUsuario/Main.java
@@ -7,29 +7,7 @@ import usuario.Usuario;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner (System.in);
-
-		System.out.println("Nome:");
-		String nome = sc.nextLine();
-		System.out.println("Id:");
-		String id = sc.nextLine();
-		System.out.println("Auditor:");
-		String auditor = sc.nextLine();
-		
-		Usuario usuario = new Usuario(nome, id, auditor);
-		
-	}
-	
-	public static String opcoesGerais() {
-		return "(U)suário" + System.lineSeparator() + 
-				"(M)áquina";
-	}
-	
-	public static String opcoesUsuario() {
-		return "(C)adastrar Usuário" + System.lineSeparator() +
-		"(A)tualizar Usuário" + System.lineSeparator() +
-		"(R)emover Usuário" + System.lineSeparator() ;
-		
+		System.out.println("Sem interface por enquanto!");
 	}
 
 }
