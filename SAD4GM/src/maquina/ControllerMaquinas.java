5b789cf82a872e28af798eb92a3fefa2972d687a
caiosbl <caio.lira@ccc.ufcg.edu.br>
2017-10-26 15:48:53 -0300

    Adicionado controlador de Máquinas

---
+++ SAD4GM/src/maquina/ControllerMaquinas.java
@@ -0,0 +1,23 @@
+package maquina;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * UNIVERSIDADE FEDERAL DE CAMPINA GRANDE - LABORATÓRIO DESIDES SISTEMA SAD4GM
+ * 
+ * @author caiosbl
+ *
+ */
+
+public class ControllerMaquinas {
+	
+	Map mapaDeMaquinas;
+	
+	public ControllerMaquinas() {
+		this.mapaDeMaquinas = new HashMap<>();
+	}
+	
+	
+
+}
