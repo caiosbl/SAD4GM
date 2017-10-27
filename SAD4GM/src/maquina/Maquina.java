8f26d4953b4d69ce1bdbd4f342149329ba6cac01
caiosbl <caio.lira@ccc.ufcg.edu.br>
2017-10-27 15:17:40 -0300

    Adicionadas classes de Máquina

--- SAD4GM/src/maquina/Maquina.java
+++ SAD4GM/src/maquina/Maquina.java
@@ -1,5 +1,10 @@
 package maquina;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import maquina.subsistema.Subsistema;
+
 /**
  * UNIVERSIDADE FEDERAL DE CAMPINA GRANDE - LABORATÓRIO DESIDES SISTEMA SAD4GM
  * 
@@ -8,5 +13,43 @@ package maquina;
  */
 
 public class Maquina {
-// A ser implementada
+
+	private String nome;
+	private int codigo;
+	private String descricao;
+
+	private Map<String,Subsistema> mapaDeSubsistemas;
+	
+	public Maquina(String nome, int codigo, String descricao) {
+		this.nome = nome;
+		this.codigo = codigo;
+		this.descricao = descricao;
+		this.mapaDeSubsistemas = new HashMap<>();
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	
 }
