1edfa0d88426a4a45d853755728c88e6253fdc56
caiosbl <caio.lira@ccc.ufcg.edu.br>
2017-10-27 15:48:02 -0300

    Em implementação Atualiza Máquina

--- SAD4GM/src/maquina/ControllerMaquinas.java
+++ SAD4GM/src/maquina/ControllerMaquinas.java
@@ -20,46 +20,59 @@ public class ControllerMaquinas {
 
 	private Map<Integer, Maquina> mapaDeMaquinas;
 
-
-
 	public ControllerMaquinas() {
 		this.mapaDeMaquinas = new HashMap<>();
 	}
-	
+
 	private void verificaCodigo(int codigo) {
-		if(mapaDeMaquinas.containsKey(codigo)) {
+		if (mapaDeMaquinas.containsKey(codigo)) {
 			throw new RuntimeErrorException(null, "CÓDIGO DE MÁQUINA JÁ CADASTRADA!");
 		}
 	}
-	
+
 	private void validaCodigo(int codigo) {
-		if(!mapaDeMaquinas.containsKey(codigo)) {
+		if (!mapaDeMaquinas.containsKey(codigo)) {
 			throw new RuntimeErrorException(null, "MÁQUINA NÃO CADASTRADA!");
 		}
 	}
-	
+
 	public void adicionaMaquina(String nome, int codigo, String descricao) {
 		verificaCodigo(codigo);
 		Maquina maquina = new Maquina(nome, codigo, descricao);
 		mapaDeMaquinas.put(maquina.getCodigo(), maquina);
 	}
-	
+
 	public void removerMaquina(int codigo) {
 		validaCodigo(codigo);
 		mapaDeMaquinas.remove(codigo);
 	}
-	
-	public void atualizarMaquina(int codigo, String dado,String novoValor) {
+
+	public void atualizarMaquina(int codigo, String dado, String novoValor) {
 		validaCodigo(codigo);
 
 		Maquina maquina = mapaDeMaquinas.get(codigo);
 
 		final AtributosMaquina atributo;
-		
+
 		try {
 			atributo = AtributosMaquina.valueOf(dado.toUpperCase());
 		} catch (IllegalArgumentException e) {
 			throw new IllegalArgumentException("DADO A SER ATUALIZADO INVÁLIDO!");
+		}
+
+		switch (atributo) {
+		case NOME:
+			maquina.setNome(novoValor);
+
+			break;
+		case CÓDIGO:
+			break;
+
+		case DESCRIÇÃO:
+			break;
+
+		default:
+			break;
 		}
 	}
 
