707ee47020dc8acecff310afe0f42b792776672b
caiosbl <caio.lira@ccc.ufcg.edu.br>
2017-11-09 15:16:05 -0300

    Criando Banco de Dados

---
+++ SAD4GM/scripts/create.sql
@@ -0,0 +1,12 @@
+create table sad4gm.usuario(
+nome VARCHAR(20) NOT NULL,
+id VARCHAR(20)NOT NULL,
+senha INTEGER NOT NULL,
+auditor VARCHAR(20)NOT NULL);
+
+create table sad4gm.maquina(
+nome VARCHAR(20) NOT NULL,
+codigo INTEGER NOT NULL,
+descricao VARCHAR(20)NOT NULL)
+
+
