589c035e51475f07f73b6442e2308d248978cceb
caiosbl <caio.lira@ccc.ufcg.edu.br>
2017-11-28 16:26:00 -0300

    Classe Controller Admins finalizada

--- SAD4GM/src/controllers/ControllerAdmins.java
+++ SAD4GM/src/controllers/ControllerAdmins.java
@@ -40,6 +40,80 @@ public class ControllerAdmins {
 		return status;
 	}
 
+	public String deletar(String id) {
+		String status;
+
+		try {
+			admTools.delete(id);
+			status = "Admin Removido com Sucesso!";
+		} catch (Exception e) {
+			status = e.getMessage();
+		}
+
+		return status;
+	}
+
+	public String setNome(String nome, String id) {
+		String status;
+
+		try {
+			ValidaUsuario.validaNome(nome);
+			admTools.setNome(nome, id);
+			status = "Nome alterado com Sucesso!";
+		} catch (Exception e) {
+			status = e.getMessage();
+		}
+
+		return status;
+	}
+
+	public String setId(String id, String novoId) {
+		String status;
+
+		try {
+			ValidaUsuario.validaId(novoId);
+			admTools.setId(id, novoId);
+			status = "Nome alterado com Sucesso!";
+		} catch (Exception e) {
+			status = e.getMessage();
+		}
+
+		return status;
+	}
+
+	public String setSenha(String id, String senha) {
+		String status;
+		int senhaInt;
+
+		try {
+			senhaInt = Integer.parseInt(senha);
+		} catch (Exception e) {
+			return "Senha Inválida!";
+		}
+
+		try {
+
+			admTools.setSenha(id, senhaInt);
+			status = "Senha alterada com Sucesso!";
+		} catch (Exception e) {
+			status = e.getMessage();
+		}
+
+		return status;
+	}
+
+	public String getInfo(String id) {
+		String info;
+
+		try {
+			info = admTools.getInfo(id);
+		} catch (Exception e) {
+			info = e.getMessage();
+		}
+
+		return info;
+	}
+
 	public boolean autentica(String id, String senha) throws SQLException {
 		int senhaInt;
 		boolean status;
