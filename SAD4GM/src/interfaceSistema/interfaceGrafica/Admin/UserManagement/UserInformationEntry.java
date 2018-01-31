c195db60fa24865be7f99709b0a2b3702f711118
caiosbl <caio.lira@ccc.ufcg.edu.br>
2018-01-31 10:39:06 -0300

    Fim da Implementação do User Management Tools

---
+++ SAD4GM/src/interfaceSistema/interfaceGrafica/Admin/UserManagement/UserInformationEntry.java
@@ -0,0 +1,145 @@
+package interfaceSistema.interfaceGrafica.Admin.UserManagement;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import sistema.Sistema;
+
+import javax.swing.JDesktopPane;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.SystemColor;
+import javax.swing.JSeparator;
+import javax.swing.JTextField;
+
+public class UserInformationEntry extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1728238218376528571L;
+	private JPanel contentPane;
+	private String idAdmin;
+	private JTextField idField;
+	private Sistema sistema = new Sistema();
+
+	/**
+	 * Launch the application.
+	 */
+
+	/**
+	 * Create the frame.
+	 */
+	public UserInformationEntry(String id) {
+		this.idAdmin = id;
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("SAD4GM");
+		setResizable(false);
+		setBounds(100, 100, 621, 497);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+
+		JDesktopPane desktopPane = new JDesktopPane();
+		desktopPane.setBackground(SystemColor.inactiveCaption);
+		contentPane.add(desktopPane, BorderLayout.CENTER);
+		desktopPane.setLayout(null);
+
+		JLabel label_1 = new JLabel("DeSiDeS");
+		label_1.setBounds(20, 60, 141, 45);
+		label_1.setForeground(Color.WHITE);
+		label_1.setFont(new Font("Tahoma", Font.BOLD, 28));
+		desktopPane.add(label_1);
+
+		JLabel label_2 = new JLabel("SAD4GM");
+		label_2.setBounds(10, 11, 210, 73);
+		label_2.setForeground(Color.WHITE);
+		label_2.setFont(new Font("Tahoma", Font.BOLD, 37));
+		desktopPane.add(label_2);
+
+		JSeparator separator = new JSeparator();
+		separator.setBounds(0, 111, 605, 12);
+		desktopPane.add(separator);
+
+		JLabel lblRemover = new JLabel("VER INFORMAÇÕES");
+		lblRemover.setForeground(Color.WHITE);
+		lblRemover.setFont(new Font("Tahoma", Font.BOLD, 30));
+		lblRemover.setBounds(221, 15, 297, 37);
+		desktopPane.add(lblRemover);
+
+		JLabel lblAdmin = new JLabel(" DE UM USUÁRIO");
+		lblAdmin.setForeground(Color.WHITE);
+		lblAdmin.setFont(new Font("Tahoma", Font.BOLD, 30));
+		lblAdmin.setBounds(243, 47, 261, 37);
+		desktopPane.add(lblAdmin);
+
+		JButton button = new JButton("Voltar");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				UserManagementOptions umgOptions = new UserManagementOptions(idAdmin);
+
+				dispose();
+				umgOptions.setVisible(true);
+				umgOptions.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			}
+		});
+		button.setFont(new Font("Tahoma", Font.BOLD, 12));
+		button.setBounds(506, 388, 65, 27);
+		desktopPane.add(button);
+
+		idField = new JTextField();
+		idField.setBounds(200, 248, 206, 28);
+		desktopPane.add(idField);
+		idField.setColumns(10);
+
+		JLabel lblIdDoAdmin = new JLabel("ID do Usuário:");
+		lblIdDoAdmin.setForeground(Color.WHITE);
+		lblIdDoAdmin.setFont(new Font("SansSerif", Font.BOLD, 14));
+		lblIdDoAdmin.setBounds(250, 222, 98, 19);
+		desktopPane.add(lblIdDoAdmin);
+
+		JButton btnRemover = new JButton("Visualizar");
+		btnRemover.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (idField.getText().trim().length() < 4) {
+					JOptionPane.showMessageDialog(null, "ID Inválido!");
+					idField.setText("");
+				} else {
+					boolean has = false;
+
+					try {
+						has = sistema.hasIdUsuario(idField.getText().trim());
+					} catch (Exception e1) {
+						JOptionPane.showMessageDialog(null, "Falha na conexão com banco de dados!");
+					}
+
+					if (!has) {
+						JOptionPane.showMessageDialog(null, "Usuário inexistente!");
+						idField.setText("");
+					}
+
+					else {
+						UserInformation userInformation = new UserInformation(idAdmin,idField.getText().trim());
+						dispose();
+						userInformation.setVisible(true);
+						userInformation.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+					}
+				}
+			}
+		});
+		btnRemover.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnRemover.setBounds(311, 280, 95, 27);
+		desktopPane.add(btnRemover);
+
+	}
+}
