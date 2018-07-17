751145cba884e5074b655def49fd2df1ffd0a267
caiosbl <caio.lira@ccc.ufcg.edu.br>
2018-07-17 08:47:36 -0300

    Icones de Renderizar Tree atualizados

--- SAD4GM/Front-end/interfaceGrafica/utils/RenderizarTree.java
+++ SAD4GM/Front-end/interfaceGrafica/utils/RenderizarTree.java
@@ -1,10 +1,9 @@
 package interfaceGrafica.utils;
 
 import java.awt.Component;
-import java.awt.Font;
 
 import javax.swing.ImageIcon;
-import javax.swing.JFrame;
+
 import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeCellRenderer;
@@ -15,55 +14,53 @@ import entidades.Falha;
 import entidades.Maquina;
 import entidades.ModoFalha;
 import entidades.Subsistema;
-import interfaceGrafica.usuario.gerenciadorMaquinas.ViewComponente;
-import interfaceGrafica.usuario.gerenciadorMaquinas.ViewFalha;
-import interfaceGrafica.usuario.gerenciadorMaquinas.ViewMachine;
-import interfaceGrafica.usuario.gerenciadorMaquinas.ViewModoFalha;
-import interfaceGrafica.usuario.gerenciadorMaquinas.ViewSubsistema;
+
 
 public class RenderizarTree extends DefaultTreeCellRenderer implements TreeCellRenderer {
-    /**
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -9027400418658050779L;
-	private Font plainFont, italicFont;
-   
-    @Override
-    public Component getTreeCellRendererComponent(JTree tree, Object value, boolean selected, boolean expanded, boolean leaf, int row, boolean hasFocus) {
-        super.getTreeCellRendererComponent(tree, value, selected, expanded, leaf, row, hasFocus);
-        DefaultMutableTreeNode node = (DefaultMutableTreeNode) value;
-        final Class<? extends Object> CLASS_TYPE = node.getUserObject().getClass();
-       
-        if(node.toString().equals("Máquinas")) {
-            setIcon(new ImageIcon(ViewMachine.class.getResource("/Resources/icon/tree/machines.png")));
-        }
-        else if(node.toString().equals("Subsistemas")) {
-            setIcon(new ImageIcon(ViewMachine.class.getResource("/Resources/icon/tree/subsystems.png")));
-        }
-        
-        else if (CLASS_TYPE == Maquina.class) {
-        	 setIcon(new ImageIcon(ViewMachine.class.getResource("/Resources/icon/tree/machine.png")));
-		}
 
-		else if (CLASS_TYPE == Subsistema.class) {
-			 setIcon(new ImageIcon(ViewMachine.class.getResource("/Resources/icon/tree/subsystem.png")));
-		}
+	@Override
+	public Component getTreeCellRendererComponent(JTree tree, Object value, boolean selected, boolean expanded,
+			boolean leaf, int row, boolean hasFocus) {
+		super.getTreeCellRendererComponent(tree, value, selected, expanded, leaf, row, hasFocus);
+		DefaultMutableTreeNode node = (DefaultMutableTreeNode) value;
+		final Class<? extends Object> CLASS_TYPE = node.getUserObject().getClass();
 
-		else if (CLASS_TYPE == Componente.class) {
+		if (node.toString().equals("Máquinas")) 
+			setIcon(new ImageIcon(RenderizarTree.class.getResource("/Resources/icon/tree/machines.png")));
+		else if (node.toString().equals("Subsistemas")) 
+			setIcon(new ImageIcon(RenderizarTree.class.getResource("/Resources/icon/tree/subsystems.png")));
 		
-		}
+		else if (node.toString().equals("Componentes")) 
+			setIcon(new ImageIcon(RenderizarTree.class.getResource("/Resources/icon/tree/components.png")));
+		
+		else if (node.toString().equals("Falhas")) 
+			setIcon(new ImageIcon(RenderizarTree.class.getResource("/Resources/icon/tree/falhas.png")));
+		
+		else if (node.toString().equals("Modos de Falha")) 
+			setIcon(new ImageIcon(RenderizarTree.class.getResource("/Resources/icon/tree/modosfalha.png")));
+		
+		
+		else if (CLASS_TYPE == Maquina.class) 
+			setIcon(new ImageIcon(RenderizarTree.class.getResource("/Resources/icon/tree/machine.png")));
 
-		else if (CLASS_TYPE == Falha.class) {
-			
-		}
+		else if (CLASS_TYPE == Subsistema.class) 
+			setIcon(new ImageIcon(RenderizarTree.class.getResource("/Resources/icon/tree/subsystem.png")));
 
-		else if (CLASS_TYPE == ModoFalha.class) {
+		else if (CLASS_TYPE == Componente.class) 
+			setIcon(new ImageIcon(RenderizarTree.class.getResource("/Resources/icon/tree/component.png")));
 
-		}
-        
-        
-        return this;
-    }
+		else if (CLASS_TYPE == Falha.class) 
+			setIcon(new ImageIcon(RenderizarTree.class.getResource("/Resources/icon/tree/falha.png")));
+		
+		else if (CLASS_TYPE == ModoFalha.class) 
+			setIcon(new ImageIcon(RenderizarTree.class.getResource("/Resources/icon/tree/modofalha.png")));
+		
 
-	
+		return this;
+	}
+
 }
