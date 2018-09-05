f2e4f610c4a4b424a159deaf0dd5a404d4224343
caiosbl <caio.lira@ccc.ufcg.edu.br>
2018-09-05 10:46:57 -0300

    Add Navbar ViewMachine

--- SAD4GM/Front-end/interfaceGrafica/usuario/gerenciadorMaquinas/ViewMachine.java
+++ SAD4GM/Front-end/interfaceGrafica/usuario/gerenciadorMaquinas/ViewMachine.java
@@ -12,6 +12,9 @@ import javax.swing.border.EmptyBorder;
 
 import entidades.Maquina;
 import interfaceGrafica.main.Main;
+import interfaceGrafica.usuario.entrada.Login;
+import interfaceGrafica.usuario.entrada.MyInfo;
+import interfaceGrafica.usuario.entrada.Options;
 import sistema.Sistema;
 
 import javax.swing.JDesktopPane;
@@ -138,11 +141,6 @@ public class ViewMachine extends Main {
 		descricaoPane.setText(String.valueOf(maquina.getDescricao()));
 		descricaoPane.setForeground(Color.BLACK);
 
-		JLabel logo = new JLabel("");
-		logo.setBounds(29, 40, 205, 74);
-		logo.setIcon(new ImageIcon(ViewMachine.class.getResource("/Resources/icon/sad4logosmall.png")));
-		desktopPane.add(logo);
-
 		JTextPane usuarioCadastrouPane = new JTextPane();
 		usuarioCadastrouPane.setEditable(false);
 		usuarioCadastrouPane.setBounds(185, 363, 332, 29);
@@ -155,17 +153,73 @@ public class ViewMachine extends Main {
 		form.setIcon(new ImageIcon(ViewMachine.class.getResource("/Resources/icon/newform.png")));
 		desktopPane.add(form);
 		
-		JLabel label = new JLabel("VISUALIZAR");
-		label.setForeground(Color.WHITE);
-		label.setFont(new Font("Tahoma", Font.BOLD, 33));
-		label.setBounds(308, 26, 209, 40);
-		desktopPane.add(label);
+		JLabel label_1 = new JLabel("VISUALIZAR");
+		label_1.setForeground(Color.WHITE);
+		label_1.setFont(new Font("Tahoma", Font.BOLD, 24));
+		label_1.setBounds(67, 22, 151, 29);
+		desktopPane.add(label_1);
 		
 		JLabel lblMquina = new JLabel("MÁQUINA");
 		lblMquina.setForeground(Color.WHITE);
-		lblMquina.setFont(new Font("Tahoma", Font.BOLD, 33));
-		lblMquina.setBounds(331, 61, 165, 40);
+		lblMquina.setFont(new Font("Tahoma", Font.BOLD, 24));
+		lblMquina.setBounds(80, 45, 120, 29);
 		desktopPane.add(lblMquina);
+		
+		JButton button = new JButton("");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Options options = new Options(id, getXLocation(), getYLocation());
+				dispose();
+				options.setIconImage(new ImageIcon(getClass().getResource("/Resources/icon/icon.png")).getImage());
+				options.setVisible(true);
+				options.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			}
+		});
+		button.setSelectedIcon(new ImageIcon(ViewMachine.class.getResource("/Resources/icon/home-on.png")));
+		button.setIcon(new ImageIcon(ViewMachine.class.getResource("/Resources/icon/home-off.png")));
+		button.setFont(new Font("Tahoma", Font.BOLD, 14));
+		button.setBackground(new Color(0, 0, 0, 0));
+		button.setBounds(349, 9, 62, 44);
+		desktopPane.add(button);
+		
+		JButton button_1 = new JButton("");
+		button_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				MyInfo myInfo = new MyInfo(id, getXLocation(), getYLocation());
+				dispose();
+				myInfo.setIconImage(new ImageIcon(getClass().getResource("/Resources/icon/icon.png")).getImage());
+				myInfo.setVisible(true);
+				myInfo.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			}
+		});
+		button_1.setSelectedIcon(new ImageIcon(ViewMachine.class.getResource("/Resources/icon/my-data-on.png")));
+		button_1.setIcon(new ImageIcon(ViewMachine.class.getResource("/Resources/icon/my-data-off.png")));
+		button_1.setFont(new Font("Tahoma", Font.BOLD, 14));
+		button_1.setBackground(new Color(0, 0, 0, 0));
+		button_1.setBounds(404, 9, 119, 45);
+		desktopPane.add(button_1);
+		
+		JButton button_2 = new JButton("");
+		button_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Login login = new Login(getXLocation(), getYLocation());
+				dispose();
+				login.setIconImage(new ImageIcon(getClass().getResource("/Resources/icon/icon.png")).getImage());
+				login.setVisible(true);
+				login.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			}
+		});
+		button_2.setSelectedIcon(new ImageIcon(ViewMachine.class.getResource("/Resources/icon/logout-on.png")));
+		button_2.setIcon(new ImageIcon(ViewMachine.class.getResource("/Resources/icon/logout-off.png")));
+		button_2.setFont(new Font("Tahoma", Font.BOLD, 14));
+		button_2.setBackground(new Color(0, 0, 0, 0));
+		button_2.setBounds(499, 9, 119, 45);
+		desktopPane.add(button_2);
+		
+		JLabel label = new JLabel("");
+		label.setIcon(new ImageIcon(ViewMachine.class.getResource("/Resources/icon/navbar.png")));
+		label.setBounds(350, 6, 256, 51);
+		desktopPane.add(label);
 
 	}
 }
