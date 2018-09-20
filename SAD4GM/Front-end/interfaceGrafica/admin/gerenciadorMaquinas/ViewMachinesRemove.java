99e35e669bf80b88e3b189007328c0b7cb566714
caiosbl <caio.lira@ccc.ufcg.edu.br>
2018-09-20 14:39:45 -0300

    Layout de ViewMachinesRemove refatorado

--- SAD4GM/Front-end/interfaceGrafica/admin/gerenciadorMaquinas/ViewMachinesRemove.java
+++ SAD4GM/Front-end/interfaceGrafica/admin/gerenciadorMaquinas/ViewMachinesRemove.java
@@ -19,6 +19,10 @@ import entidades.Maquina;
 import entidades.ModoFalha;
 import entidades.Subsistema;
 import interfaceGrafica.main.Main;
+import interfaceGrafica.usuario.entrada.Login;
+import interfaceGrafica.usuario.entrada.MyInfo;
+import interfaceGrafica.usuario.entrada.Options;
+import interfaceGrafica.usuario.gerenciadorMaquinas.ViewMachinesInfo;
 import interfaceGrafica.utils.RenderizarTree;
 import interfaceGrafica.utils.Tree;
 import sistema.Sistema;
@@ -35,6 +39,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.SystemColor;
 import javax.swing.JTree;
+import java.awt.Color;
 
 /**
  * UNIVERSIDADE FEDERAL DE CAMPINA GRANDE - LABORATÓRIO DESIDES SISTEMA SAD4GM
@@ -84,7 +89,9 @@ public class ViewMachinesRemove extends Main {
 		iniciaTree();
 
 		JButton btnVoltar = new JButton("");
-		btnVoltar.setIcon(new ImageIcon(ViewMachinesRemove.class.getResource("/Resources/icon/voltabut.png")));
+		btnVoltar.setSelectedIcon(new ImageIcon(ViewMachinesRemove.class.getResource("/Resources/icon/return-selected.png")));
+		btnVoltar.setBackground(new Color(0,0,0,0));
+		btnVoltar.setIcon(new ImageIcon(ViewMachinesRemove.class.getResource("/Resources/icon/back-btn.png")));
 		btnVoltar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				MachineManagementOptions mMoptions = new MachineManagementOptions(idAdmin, getXLocation(),
@@ -96,23 +103,75 @@ public class ViewMachinesRemove extends Main {
 			}
 		});
 		btnVoltar.setFont(new Font("Tahoma", Font.BOLD, 12));
-		btnVoltar.setBounds(484, 418, 88, 29);
+		btnVoltar.setBounds(518, 411, 78, 44);
 		desktopPane.add(btnVoltar);
-
-		JLabel logo = new JLabel("");
-		logo.setIcon(new ImageIcon(ViewMachinesRemove.class.getResource("/Resources/icon/sad4logosmall.png")));
-		logo.setBounds(29, 40, 205, 74);
-		desktopPane.add(logo);
-
-		JLabel banner = new JLabel("");
-		banner.setIcon(new ImageIcon(ViewMachinesRemove.class.getResource("/Resources/icon/remove-maquina-title.png")));
-		banner.setBounds(311, 21, 214, 104);
-		desktopPane.add(banner);
-
+		
+		JLabel lblRemover = new JLabel("REMOVER");
+		lblRemover.setForeground(Color.WHITE);
+		lblRemover.setFont(new Font("Tahoma", Font.BOLD, 24));
+		lblRemover.setBounds(67, 22, 119, 29);
+		desktopPane.add(lblRemover);
+		
+		JLabel lblDadosDeMquina = new JLabel("DADOS DE MÁQUINA");
+		lblDadosDeMquina.setForeground(Color.WHITE);
+		lblDadosDeMquina.setFont(new Font("Tahoma", Font.BOLD, 24));
+		lblDadosDeMquina.setBounds(13, 49, 252, 29);
+		desktopPane.add(lblDadosDeMquina);
+		
+		JButton homeBtn = new JButton("");
+		homeBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Options options = new Options(id, getXLocation(), getYLocation());
+				dispose();
+				options.setIconImage(new ImageIcon(getClass().getResource("/Resources/icon/icon.png")).getImage());
+				options.setVisible(true);
+				options.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			}
+		});
+		homeBtn.setSelectedIcon(new ImageIcon(ViewMachinesInfo.class.getResource("/Resources/icon/home-on.png")));
+		homeBtn.setIcon(new ImageIcon(ViewMachinesInfo.class.getResource("/Resources/icon/home-off.png")));
+		homeBtn.setFont(new Font("Tahoma", Font.BOLD, 14));
+		homeBtn.setBackground(new Color(0, 0, 0, 0));
+		homeBtn.setBounds(349, 9, 62, 44);
+		desktopPane.add(homeBtn);
+		
+		JButton myDataBtn = new JButton("");
+		myDataBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				MyInfo myInfo = new MyInfo(id, getXLocation(), getYLocation());
+				dispose();
+				myInfo.setIconImage(new ImageIcon(getClass().getResource("/Resources/icon/icon.png")).getImage());
+				myInfo.setVisible(true);
+				myInfo.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			}
+		});
+		myDataBtn.setSelectedIcon(new ImageIcon(ViewMachinesInfo.class.getResource("/Resources/icon/my-data-on.png")));
+		myDataBtn.setIcon(new ImageIcon(ViewMachinesInfo.class.getResource("/Resources/icon/my-data-off.png")));
+		myDataBtn.setFont(new Font("Tahoma", Font.BOLD, 14));
+		myDataBtn.setBackground(new Color(0, 0, 0, 0));
+		myDataBtn.setBounds(404, 9, 119, 45);
+		desktopPane.add(myDataBtn);
+		
+		JButton logoutBtn = new JButton("");
+		logoutBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Login login = new Login(getXLocation(), getYLocation());
+				dispose();
+				login.setIconImage(new ImageIcon(getClass().getResource("/Resources/icon/icon.png")).getImage());
+				login.setVisible(true);
+				login.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			}
+		});
+		logoutBtn.setSelectedIcon(new ImageIcon(ViewMachinesInfo.class.getResource("/Resources/icon/logout-on.png")));
+		logoutBtn.setIcon(new ImageIcon(ViewMachinesInfo.class.getResource("/Resources/icon/logout-off.png")));
+		logoutBtn.setFont(new Font("Tahoma", Font.BOLD, 14));
+		logoutBtn.setBackground(new Color(0, 0, 0, 0));
+		logoutBtn.setBounds(499, 9, 119, 45);
+		desktopPane.add(logoutBtn);
+		
 		JLabel label = new JLabel("");
-		label.setIcon(
-				new ImageIcon(ViewMachinesRemove.class.getResource("/Resources/icon/top-select-item-remove.png")));
-		label.setBounds(69, 138, 460, 25);
+		label.setIcon(new ImageIcon(ViewMachinesInfo.class.getResource("/Resources/icon/navbar.png")));
+		label.setBounds(350, 6, 256, 51);
 		desktopPane.add(label);
 
 	}
@@ -231,8 +290,7 @@ public class ViewMachinesRemove extends Main {
 
 		tree.addTreeSelectionListener(tsl);
 		JScrollPane jSPane = new JScrollPane(tree);
-		jSPane.setBounds(67, 161, 465, 244);
+		jSPane.setBounds(13, 139, 581, 270);
 		desktopPane.add(jSPane);
 	}
-
 }
