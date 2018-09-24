c927eef2cabddfd29e8426ffe8ac830824cb38bf
caiosbl <caio.lira@ccc.ufcg.edu.br>
2018-09-24 10:50:40 -0300

    Layout de SetComponente Refatorado

--- SAD4GM/Front-end/interfaceGrafica/admin/gerenciadorMaquinas/editMachines/SetComponente.java
+++ SAD4GM/Front-end/interfaceGrafica/admin/gerenciadorMaquinas/editMachines/SetComponente.java
@@ -1,6 +1,8 @@
 package interfaceGrafica.admin.gerenciadorMaquinas.editMachines;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -8,6 +10,11 @@ import javax.swing.border.EmptyBorder;
 
 import interfaceGrafica.admin.gerenciadorMaquinas.ViewMachinesEdit;
 import interfaceGrafica.main.Main;
+import interfaceGrafica.usuario.entrada.Login;
+import interfaceGrafica.usuario.entrada.MyInfo;
+import interfaceGrafica.usuario.entrada.Options;
+import interfaceGrafica.usuario.gerenciadorMaquinas.ViewComponente;
+import interfaceGrafica.usuario.gerenciadorMaquinas.ViewSubsistema;
 
 import javax.swing.JDesktopPane;
 import javax.swing.JLabel;
@@ -18,7 +25,6 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.SystemColor;
-import javax.swing.JSeparator;
 import javax.swing.JTextPane;
 
 import sistema.Sistema;
@@ -65,13 +71,11 @@ public class SetComponente extends Main {
 		contentPane.add(desktopPane, BorderLayout.CENTER);
 		desktopPane.setLayout(null);
 
-		JSeparator separator = new JSeparator();
-		separator.setBounds(10, 137, 582, 12);
-		desktopPane.add(separator);
-
 		JButton btnVoltar = new JButton("");
-		btnVoltar.setIcon(new ImageIcon(SetComponente.class.getResource("/Resources/icon/voltabut.png")));
-		btnVoltar.setBounds(490, 420, 90, 27);
+		btnVoltar.setSelectedIcon(new ImageIcon(SetComponente.class.getResource("/Resources/icon/return-selected.png")));
+		btnVoltar.setBackground(new Color(0,0,0,0));
+		btnVoltar.setIcon(new ImageIcon(SetComponente.class.getResource("/Resources/icon/back-btn.png")));
+		btnVoltar.setBounds(508, 393, 78, 44);
 		btnVoltar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				ViewMachinesEdit viewMachinesEdit = new ViewMachinesEdit(idUsuario, getXLocation(), getYLocation());
@@ -84,31 +88,77 @@ public class SetComponente extends Main {
 		desktopPane.add(btnVoltar);
 
 		JTextPane nome = new JTextPane();
-		nome.setEditable(false);
-		nome.setBounds(190, 200, 264, 24);
+		nome.setBounds(46, 190, 518, 35);
 		nome.setText(sistema.getNomeComponente(chaveComponente));
 		desktopPane.add(nome);
 
 		JTextPane funcaoPane = new JTextPane();
 		funcaoPane.setText(sistema.getFuncaoComponente(chaveComponente));
 		JScrollPane jPane = new JScrollPane(funcaoPane);
-		jPane.setBounds(188, 240, 268, 80);
-		funcaoPane.setEditable(false);
+		jPane.setBounds(44, 240, 521, 100);
 		desktopPane.add(jPane);
-
-		JButton btnEditar = new JButton("");
-		btnEditar.setIcon(new ImageIcon(SetComponente.class.getResource("/Resources/icon/editbutton.png")));
-		btnEditar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				nome.setEditable(true);
-				funcaoPane.setEditable(true);
+		
+		JButton homeBtn = new JButton("");
+		homeBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Options options = new Options(idUser, getXLocation(), getYLocation());
+				dispose();
+				options.setIconImage(new ImageIcon(getClass().getResource("/Resources/icon/icon.png")).getImage());
+				options.setVisible(true);
+				options.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 			}
 		});
-		btnEditar.setBounds(188, 325, 90, 28);
-		desktopPane.add(btnEditar);
+		homeBtn.setSelectedIcon(new ImageIcon(ViewComponente.class.getResource("/Resources/icon/home-on.png")));
+		homeBtn.setIcon(new ImageIcon(ViewComponente.class.getResource("/Resources/icon/home-off.png")));
+		homeBtn.setFont(new Font("Tahoma", Font.BOLD, 14));
+		homeBtn.setBackground(new Color(0, 0, 0, 0));
+		homeBtn.setBounds(349, 9, 62, 44);
+		desktopPane.add(homeBtn);
+
+		JButton myDataBtn = new JButton("");
+		myDataBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				MyInfo myInfo = new MyInfo(idUser, getXLocation(), getYLocation());
+				dispose();
+				myInfo.setIconImage(new ImageIcon(getClass().getResource("/Resources/icon/icon.png")).getImage());
+				myInfo.setVisible(true);
+				myInfo.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			}
+		});
+		myDataBtn.setSelectedIcon(new ImageIcon(ViewSubsistema.class.getResource("/Resources/icon/my-data-on.png")));
+		myDataBtn.setIcon(new ImageIcon(ViewSubsistema.class.getResource("/Resources/icon/my-data-off.png")));
+		myDataBtn.setFont(new Font("Tahoma", Font.BOLD, 14));
+		myDataBtn.setBackground(new Color(0, 0, 0, 0));
+		myDataBtn.setBounds(404, 9, 119, 45);
+		desktopPane.add(myDataBtn);
+
+		JButton logoutBtn = new JButton("");
+		logoutBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Login login = new Login(getXLocation(), getYLocation());
+				dispose();
+				login.setIconImage(new ImageIcon(getClass().getResource("/Resources/icon/icon.png")).getImage());
+				login.setVisible(true);
+				login.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			}
+		});
+		logoutBtn.setSelectedIcon(new ImageIcon(ViewSubsistema.class.getResource("/Resources/icon/logout-on.png")));
+		logoutBtn.setIcon(new ImageIcon(ViewSubsistema.class.getResource("/Resources/icon/logout-off.png")));
+		logoutBtn.setFont(new Font("Tahoma", Font.BOLD, 14));
+		logoutBtn.setBackground(new Color(0, 0, 0, 0));
+		logoutBtn.setBounds(499, 9, 119, 45);
+		desktopPane.add(logoutBtn);
+
+		JLabel label = new JLabel("");
+		label.setIcon(new ImageIcon(ViewComponente.class.getResource("/Resources/icon/navbar.png")));
+		label.setBounds(350, 6, 256, 51);
+		desktopPane.add(label);
+
 
 		JButton btnAtualizar = new JButton("");
-		btnAtualizar.setIcon(new ImageIcon(SetComponente.class.getResource("/Resources/icon/atualizarbutton.png")));
+		btnAtualizar.setSelectedIcon(new ImageIcon(SetComponente.class.getResource("/Resources/icon/update-selected.png")));
+		btnAtualizar.setBackground(new Color(0,0,0,0));
+		btnAtualizar.setIcon(new ImageIcon(SetComponente.class.getResource("/Resources/icon/update.png")));
 		btnAtualizar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 
@@ -132,23 +182,20 @@ public class SetComponente extends Main {
 
 		});
 
-		btnAtualizar.setBounds(362, 325, 90, 28);
+		btnAtualizar.setBounds(36, 352, 124, 39);
 		desktopPane.add(btnAtualizar);
-
-		JLabel logo = new JLabel("");
-		logo.setIcon(new ImageIcon(SetComponente.class.getResource("/Resources/icon/sad4logosmall.png")));
-		logo.setBounds(29, 40, 205, 74);
-		desktopPane.add(logo);
-
-		JLabel banner = new JLabel("");
-		banner.setIcon(new ImageIcon(SetComponente.class.getResource("/Resources/icon/edit-component-info.png")));
-		banner.setBounds(274, 28, 286, 90);
-		desktopPane.add(banner);
-
-		JLabel label = new JLabel("");
-		label.setIcon(new ImageIcon(SetComponente.class.getResource("/Resources/icon/edit-component-info-form.png")));
-		label.setBounds(108, 172, 393, 225);
-		desktopPane.add(label);
+		
+		JLabel label_1 = new JLabel("EDITAR");
+		label_1.setForeground(Color.WHITE);
+		label_1.setFont(new Font("Tahoma", Font.BOLD, 24));
+		label_1.setBounds(67, 22, 110, 29);
+		desktopPane.add(label_1);
+		
+		JLabel lblComponente = new JLabel("COMPONENTE");
+		lblComponente.setForeground(Color.WHITE);
+		lblComponente.setFont(new Font("Tahoma", Font.BOLD, 24));
+		lblComponente.setBounds(36, 46, 172, 29);
+		desktopPane.add(lblComponente);
 
 	}
 
