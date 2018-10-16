e945b04ce51b49fca8ad80848294dcb8623c7505
caiosbl <caio.lira@ccc.ufcg.edu.br>
2018-10-16 14:57:53 -0300

    Implementada Classe View Ação Recomendada

---
+++ SAD4GM/Front-end/interfaceGrafica/usuario/gerenciadorMaquinas/ViewAcaoRecomendada.java
@@ -0,0 +1,188 @@
+package interfaceGrafica.usuario.gerenciadorMaquinas;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.border.EmptyBorder;
+
+import interfaceGrafica.main.Main;
+import interfaceGrafica.usuario.entrada.Login;
+import interfaceGrafica.usuario.entrada.MyInfo;
+import interfaceGrafica.usuario.entrada.Options;
+import sistema.Sistema;
+
+import javax.swing.JDesktopPane;
+import javax.swing.JLabel;
+
+import java.awt.Font;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.SystemColor;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+
+/**
+ * UNIVERSIDADE FEDERAL DE CAMPINA GRANDE - LABORATÓRIO DESIDES SISTEMA SAD4GM
+ * 
+ * @author caiosbl
+ *
+ */
+
+public class ViewAcaoRecomendada extends Main {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1728238218376528571L;
+	private JPanel contentPane;
+	private String idAdmin;
+
+	private Sistema sistema = new Sistema();
+
+	/**
+	 * Launch the application.
+	 */
+
+	/**
+	 * Create the frame.
+	 */
+	public ViewAcaoRecomendada(String id, int chaveAcaoRecomendada, int xLocation, int yLocation) {
+		super(xLocation, yLocation);
+		this.idAdmin = id;
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("SAD4GM");
+		setResizable(false);
+		setBounds(xLocation, yLocation, 621, 497);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+
+		JDesktopPane desktopPane = new JDesktopPane();
+		desktopPane.setBackground(SystemColor.inactiveCaption);
+		contentPane.add(desktopPane, BorderLayout.CENTER);
+		desktopPane.setLayout(null);
+
+		JButton btnVoltar = new JButton("");
+		btnVoltar.setBackground(new Color(0,0,0,0));
+		btnVoltar.setSelectedIcon(new ImageIcon(ViewAcaoRecomendada.class.getResource("/Resources/icon/return-selected.png")));
+		btnVoltar.setIcon(new ImageIcon(ViewAcaoRecomendada.class.getResource("/Resources/icon/back-btn.png")));
+		btnVoltar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ViewMachinesInfo information = new ViewMachinesInfo(idAdmin, getXLocation(), getYLocation());
+
+				dispose();
+				information.setIconImage(new ImageIcon(getClass().getResource("/Resources/icon/icon.png")).getImage());
+				information.setVisible(true);
+				information.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			}
+		});
+		btnVoltar.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnVoltar.setBounds(495, 409, 78, 44);
+		desktopPane.add(btnVoltar);
+
+		JScrollPane jsp = new JScrollPane();
+		jsp.setBounds(35, 226, 534, 116);
+		desktopPane.add(jsp);
+		
+				JTextPane descricaoPane = new JTextPane();
+				jsp.setViewportView(descricaoPane);
+				descricaoPane.setText(sistema.getDescricaoAcaoRecomendada(chaveAcaoRecomendada));
+				descricaoPane.setEditable(false);
+		
+
+		JTextField tituloField = new JTextField();
+		tituloField.setEditable(false);
+		tituloField.setText(sistema.getNomeAcaoRecomendada(chaveAcaoRecomendada));
+		tituloField.setBounds(35, 170, 534, 34);
+		desktopPane.add(tituloField);
+		tituloField.setColumns(10);
+		
+
+		JButton homeBtn = new JButton("");
+		homeBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Options options = new Options(id, getXLocation(), getYLocation());
+				dispose();
+				options.setIconImage(new ImageIcon(getClass().getResource("/Resources/icon/icon.png")).getImage());
+				options.setVisible(true);
+				options.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			}
+		});
+		homeBtn.setSelectedIcon(new ImageIcon(ViewComponente.class.getResource("/Resources/icon/home-on.png")));
+		homeBtn.setIcon(new ImageIcon(ViewComponente.class.getResource("/Resources/icon/home-off.png")));
+		homeBtn.setFont(new Font("Tahoma", Font.BOLD, 14));
+		homeBtn.setBackground(new Color(0, 0, 0, 0));
+		homeBtn.setBounds(349, 9, 62, 44);
+		desktopPane.add(homeBtn);
+
+		JButton myDataBtn = new JButton("");
+		myDataBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				MyInfo myInfo = new MyInfo(id, getXLocation(), getYLocation());
+				dispose();
+				myInfo.setIconImage(new ImageIcon(getClass().getResource("/Resources/icon/icon.png")).getImage());
+				myInfo.setVisible(true);
+				myInfo.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			}
+		});
+		myDataBtn.setSelectedIcon(new ImageIcon(ViewSubsistema.class.getResource("/Resources/icon/my-data-on.png")));
+		myDataBtn.setIcon(new ImageIcon(ViewSubsistema.class.getResource("/Resources/icon/my-data-off.png")));
+		myDataBtn.setFont(new Font("Tahoma", Font.BOLD, 14));
+		myDataBtn.setBackground(new Color(0, 0, 0, 0));
+		myDataBtn.setBounds(404, 9, 119, 45);
+		desktopPane.add(myDataBtn);
+
+		JButton logoutBtn = new JButton("");
+		logoutBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Login login = new Login(getXLocation(), getYLocation());
+				dispose();
+				login.setIconImage(new ImageIcon(getClass().getResource("/Resources/icon/icon.png")).getImage());
+				login.setVisible(true);
+				login.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			}
+		});
+		logoutBtn.setSelectedIcon(new ImageIcon(ViewSubsistema.class.getResource("/Resources/icon/logout-on.png")));
+		logoutBtn.setIcon(new ImageIcon(ViewSubsistema.class.getResource("/Resources/icon/logout-off.png")));
+		logoutBtn.setFont(new Font("Tahoma", Font.BOLD, 14));
+		logoutBtn.setBackground(new Color(0, 0, 0, 0));
+		logoutBtn.setBounds(499, 9, 119, 45);
+		desktopPane.add(logoutBtn);
+		JLabel navbar = new JLabel("");
+		navbar.setIcon(new ImageIcon(ViewComponente.class.getResource("/Resources/icon/navbar.png")));
+		navbar.setBounds(350, 6, 256, 51);
+		desktopPane.add(navbar);
+		
+		JLabel label = new JLabel("VISUALIZAR");
+		label.setForeground(Color.WHITE);
+		label.setFont(new Font("Tahoma", Font.BOLD, 24));
+		label.setBounds(77, 20, 151, 29);
+		desktopPane.add(label);
+		
+		JLabel lblCausaPotencial = new JLabel("AÇÃO RECOMENDADA");
+		lblCausaPotencial.setForeground(Color.WHITE);
+		lblCausaPotencial.setFont(new Font("Tahoma", Font.BOLD, 24));
+		lblCausaPotencial.setBounds(29, 43, 262, 29);
+		desktopPane.add(lblCausaPotencial);
+		
+		JLabel label_1 = new JLabel("Nome:");
+		label_1.setForeground(Color.WHITE);
+		label_1.setFont(new Font("Tahoma", Font.BOLD, 16));
+		label_1.setBounds(264, 150, 53, 20);
+		desktopPane.add(label_1);
+		
+		JLabel lblDescrio = new JLabel("Descrição:");
+		lblDescrio.setForeground(Color.WHITE);
+		lblDescrio.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblDescrio.setBounds(255, 205, 84, 20);
+		desktopPane.add(lblDescrio);
+
+	}
+}
