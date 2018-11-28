eaf6fa60038bade79171e307309a9e64f5009da1
caiosbl <caio.lira@ccc.ufcg.edu.br>
2018-11-27 23:09:48 -0300

    Adicionada View de Origem de Causa

---
+++ SAD4GM/Front-end/interfaceGrafica/usuario/gerenciadorMaquinas/ViewOrigemCausa.java
@@ -0,0 +1,170 @@
+package interfaceGrafica.usuario.gerenciadorMaquinas;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import interfaceGrafica.main.Main;
+import interfaceGrafica.usuario.entrada.Login;
+import interfaceGrafica.usuario.entrada.MyInfo;
+import interfaceGrafica.usuario.entrada.Options;
+import sistema.Sistema;
+
+import javax.swing.JDesktopPane;
+import javax.swing.JLabel;
+
+import java.awt.Font;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.SystemColor;
+
+import javax.swing.JTextPane;
+
+/**
+ * UNIVERSIDADE FEDERAL DE CAMPINA GRANDE - LABORATÓRIO DESIDES SISTEMA SAD4GM
+ * 
+ * @author caiosbl
+ *
+ */
+
+public class ViewOrigemCausa extends Main {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1728238218376528571L;
+	private JPanel contentPane;
+	private String idAdmin;
+
+	private Sistema sistema = new Sistema();
+
+	/**
+	 * Launch the application.
+	 */
+
+	/**
+	 * Create the frame.
+	 */
+	public ViewOrigemCausa(String id, int chaveOrigemCausa, int xLocation, int yLocation) {
+		super(xLocation, yLocation);
+		this.idAdmin = id;
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("SAD4GM");
+		setResizable(false);
+		setBounds(xLocation, yLocation, 621, 497);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+
+		JDesktopPane desktopPane = new JDesktopPane();
+		desktopPane.setBackground(SystemColor.inactiveCaption);
+		contentPane.add(desktopPane, BorderLayout.CENTER);
+		desktopPane.setLayout(null);
+
+		JButton btnVoltar = new JButton("");
+		btnVoltar.setSelectedIcon(new ImageIcon(ViewOrigemCausa.class.getResource("/Resources/icon/return-selected.png")));
+		btnVoltar.setBackground(new Color(0,0,0,0));
+		btnVoltar.setIcon(new ImageIcon(ViewOrigemCausa.class.getResource("/Resources/icon/back-btn.png")));
+		btnVoltar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ViewMachinesInfo information = new ViewMachinesInfo(idAdmin, getXLocation(), getYLocation());
+				dispose();
+				information.setIconImage(new ImageIcon(getClass().getResource("/Resources/icon/icon.png")).getImage());
+				information.setVisible(true);
+				information.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			}
+		});
+		btnVoltar.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnVoltar.setBounds(514, 408, 78, 44);
+		desktopPane.add(btnVoltar);
+
+		JTextPane textPane = new JTextPane();
+		textPane.setEditable(false);
+		textPane.setForeground(Color.BLACK);
+		textPane.setText(sistema.getNomeOrigem(chaveOrigemCausa));
+		textPane.setBounds(59, 205, 495, 30);
+		desktopPane.add(textPane);
+		
+		JLabel label = new JLabel("VISUALIZAR");
+		label.setForeground(Color.WHITE);
+		label.setFont(new Font("Tahoma", Font.BOLD, 24));
+		label.setBounds(67, 22, 151, 29);
+		desktopPane.add(label);
+		
+		JLabel lblSubsistema_1 = new JLabel("ORIGEM DE CAUSA");
+		lblSubsistema_1.setForeground(Color.WHITE);
+		lblSubsistema_1.setFont(new Font("Tahoma", Font.BOLD, 24));
+		lblSubsistema_1.setBounds(39, 51, 229, 29);
+		desktopPane.add(lblSubsistema_1);
+		
+		JButton button = new JButton("");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Options options = new Options(id, getXLocation(), getYLocation());
+				dispose();
+				options.setIconImage(new ImageIcon(getClass().getResource("/Resources/icon/icon.png")).getImage());
+				options.setVisible(true);
+				options.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			}
+		});
+		button.setIcon(new ImageIcon(ViewOrigemCausa.class.getResource("/Resources/icon/home-off.png")));
+		button.setSelectedIcon(new ImageIcon(ViewOrigemCausa.class.getResource("/Resources/icon/home-on.png")));
+		button.setFont(new Font("Tahoma", Font.BOLD, 14));
+		button.setBackground(new Color(0, 0, 0, 0));
+		button.setBounds(349, 9, 62, 44);
+		desktopPane.add(button);
+		
+		JButton button_1 = new JButton("");
+		button_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				MyInfo myInfo = new MyInfo(id, getXLocation(), getYLocation());
+				dispose();
+				myInfo.setIconImage(new ImageIcon(getClass().getResource("/Resources/icon/icon.png")).getImage());
+				myInfo.setVisible(true);
+				myInfo.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			}
+		});
+		button_1.setSelectedIcon(new ImageIcon(ViewOrigemCausa.class.getResource("/Resources/icon/my-data-on.png")));
+		button_1.setIcon(new ImageIcon(ViewOrigemCausa.class.getResource("/Resources/icon/my-data-off.png")));
+		button_1.setFont(new Font("Tahoma", Font.BOLD, 14));
+		button_1.setBackground(new Color(0, 0, 0, 0));
+		button_1.setBounds(404, 9, 119, 45);
+		desktopPane.add(button_1);
+		
+		JButton logoutBtn = new JButton("");
+		logoutBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Login login = new Login(getXLocation(), getYLocation());
+				dispose();
+				login.setIconImage(new ImageIcon(getClass().getResource("/Resources/icon/icon.png")).getImage());
+				login.setVisible(true);
+				login.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			}
+		});
+		logoutBtn.setSelectedIcon(new ImageIcon(ViewOrigemCausa.class.getResource("/Resources/icon/logout-on.png")));
+		logoutBtn.setIcon(new ImageIcon(ViewOrigemCausa.class.getResource("/Resources/icon/logout-off.png")));
+		logoutBtn.setFont(new Font("Tahoma", Font.BOLD, 14));
+		logoutBtn.setBackground(new Color(0, 0, 0, 0));
+		logoutBtn.setBounds(499, 9, 119, 45);
+		desktopPane.add(logoutBtn);
+		
+		JLabel label_1 = new JLabel("");
+		label_1.setIcon(new ImageIcon(ViewOrigemCausa.class.getResource("/Resources/icon/navbar.png")));
+		label_1.setBounds(350, 6, 256, 51);
+		desktopPane.add(label_1);
+		
+		JLabel lblNome = new JLabel("Nome:");
+		lblNome.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblNome.setForeground(Color.WHITE);
+		lblNome.setBounds(277, 180, 53, 20);
+		desktopPane.add(lblNome);
+
+	}
+}
